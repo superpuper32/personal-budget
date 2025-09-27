0e9fa27425ea5dd0cc71b41eb886a903c9fc41a6
superpuper32 <pochitalin_m@mail.ru>
2025-09-27 20:44:39 +0300

    endpoint create envelope added

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
 
+const { envelopes } = require("./data");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/envelopes", (req, res, next) => {
+    const { budget, title } = req.query;
+
+    if (budget && title) {
+      const envelopeID = envelopes.length + 1;
+      const envelopeObject = { id: envelopeID, budget, title };
+      envelopes.push(envelopeObject);
+
+      res.status(201).send({ envelope: envelopeObject });
+    } else {
+      res.status(403).send();
+    }
 });
 
 module.exports = {
---
+++ data.js
@@ -0,0 +1,16 @@
+const envelopes = [
+  {
+    id: 1,
+    title: "",
+    buget: "10.0",
+  },
+  {
+    id: 2,
+    title: "",
+    buget: "11.0",
+  },
+];
+
+module.exports = {
+  envelopes,
+};
